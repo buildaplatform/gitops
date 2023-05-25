cbc0f40e2754815ecc5a91401fca813fc7e01305
Mark Anderson-Trocme <mark.anderson@altitude-sports.com>
2023-05-25 19:46:12 -0400

    create secret with tf

--- infrastructure/tunnel/main.tf
+++ infrastructure/tunnel/main.tf
@@ -18,9 +18,11 @@ resource "kubernetes_secret" "this" {
   }
 
   data = {
-    AccountTag   = base64encode(var.account_name)
-    TunnelID     = base64encode(cloudflare_record.this.id)
-    TunnelName   = base64encode(cloudflare_tunnel.this.name)
-    TunnelSecret = base64encode(var.tunnel_secret)
+    "tunnel-config.json" = templatefile("${path.module}/tunnel-config.json", {
+      ACCOUNT_NAME  = var.account_name,
+      TUNNEL_ID     = cloudflare_record.this.id,
+      TUNNEL_NAME   = cloudflare_tunnel.this.name,
+      TUNNEL_SECRET = cloudflare_tunnel.this.tunnel_token,
+    })
   }
 }
