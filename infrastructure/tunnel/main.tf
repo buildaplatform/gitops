b736baeb644e08e71850991884ef918a57d58d3e
Mark Anderson-Trocme <mark.anderson@altitude-sports.com>
2023-05-25 20:42:31 -0400

    try ClusterIP ingress-nginx

--- infrastructure/tunnel/main.tf
+++ infrastructure/tunnel/main.tf
@@ -19,10 +19,10 @@ resource "kubernetes_secret" "this" {
 
   data = {
     "tunnel-config.json" = templatefile("${path.module}/tunnel-config.json", {
-      ACCOUNT_NAME  = var.account_name,
-      TUNNEL_ID     = cloudflare_record.this.id,
+      ACCOUNT_TAG   = var.account_id,
+      TUNNEL_ID     = cloudflare_tunnel.this.id,
       TUNNEL_NAME   = cloudflare_tunnel.this.name,
-      TUNNEL_SECRET = cloudflare_tunnel.this.tunnel_token,
+      TUNNEL_SECRET = var.tunnel_secret,
     })
   }
 }
