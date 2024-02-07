e6ac994c44547241f430daee5ef24443deade036
Mark Anderson-Trocme <mark.anderson.trocme@gmail.com>
2024-02-07 17:42:02 -0500

    add scripts for cert-manager

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,18 +5,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 make --no-print-directory ${1}
 make --no-print-directory argocd
 "${DIR}"/wait_for_deployment.sh argocd-server argocd
+
+echo "Bootstraping core Kubernetes applications... 🚜"
 kubectl apply -f bootstrap.yaml
+
 "${DIR}"/wait_for_deployment.sh ingress-nginx-controller ingress-nginx
 
+"${DIR}"/wait_for_deployment.sh cert-manager cert-manager
+"${DIR}"/wait_for_deployment.sh cert-manager-webhook cert-manager
+"${DIR}"/wait_for_deployment.sh cert-manager-cainjector cert-manager
+"${DIR}"/create_cert.sh
+
 echo ""
-echo "Argo CD"
-echo "    Visit http://localhost/argocd 🐙"
+echo "Argo CD 🐙"
+echo "    Visit https://argocd.localdev.me"
 make --no-print-directory argocd-credentials
 echo ""
 
 echo ""
-echo "Grafana"
-echo "    Visit http://localhost/grafana 📈"
+echo "Grafana 📈"
+echo "    Visit https://grafana.localdev.me"
 make --no-print-directory grafana-credentials
 echo ""
 
